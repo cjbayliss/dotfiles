cf749f53f7faa6c3a26c8351cf007012af141b0f
Christopher Bayliss <cjbdev@icloud.com>
2023-10-12 14:39:29 +1100

    switch to nushell

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -85,10 +85,12 @@ in
     podman-compose
     scrot
     tmux
+    nu_scripts
 
     # replacements written in rust
     procs # ps
     uutils-coreutils-noprefix # coreutils
+    xh # http request tool. has --download and --continue
 
     # gui
     (ungoogled-chromium.override { commandLineArgs = ''--js-flags="--jitless --noexpose_wasm" --no-pings ''; })
@@ -423,61 +425,37 @@ in
     };
   };
 
-  programs.fish = {
+  programs.nushell = {
     enable = true;
-    interactiveShellInit = ''
-      set fish_greeting
-      any-nix-shell fish | source
+    environmentVariables = {
+      # this isn't set properly under nushell
+      NIX_PATH = "/home/${config.home.username}/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
 
-      # allow urls with '?' in them
-      set -U fish_features qmark-noglob
+      # vancy colours in man pages
+      LESS_TERMCAP_mb = "(tput bold; tput setaf 1)";
+      LESS_TERMCAP_md = "(tput bold; tput setaf 1)";
+      LESS_TERMCAP_me = "(tput sgr0)";
+      LESS_TERMCAP_so = "(tput bold; tput setaf 12)";
+      LESS_TERMCAP_se = "(tput sgr0)";
+      LESS_TERMCAP_us = "(tput bold; tput setaf 14)";
+      LESS_TERMCAP_ue = "(tput sgr0)";
 
-      # colours
-      set -U fish_color_autosuggestion      brblue
-      set -U fish_color_cancel              -r
-      set -U fish_color_command             'white' '--bold'
-      set -U fish_color_comment             brblue
-      set -U fish_color_cwd                 brcyan
-      set -U fish_color_cwd_root            red
-      set -U fish_color_end                 brmagenta
-      set -U fish_color_error               brred
-      set -U fish_color_escape              brcyan
-      set -U fish_color_history_current     --bold
-      set -U fish_color_host                normal
-      set -U fish_color_match               --background=brblue
-      set -U fish_color_normal              normal
-      set -U fish_color_operator            normal
-      set -U fish_color_param               normal
-      set -U fish_color_quote               yellow
-      set -U fish_color_redirection         bryellow
-      set -U fish_color_search_match        'bryellow' '--background=brblack'
-      set -U fish_color_selection           'white' '--bold' '--background=brblack'
-      set -U fish_color_status              red
-      set -U fish_color_user                green
-      set -U fish_color_valid_path          --underline
-      set -U fish_pager_color_completion    normal
-      set -U fish_pager_color_description   yellow
-      set -U fish_pager_color_prefix        'white' '--bold' '--underline'
-      set -U fish_pager_color_progress      '-r' 'white'
+      # required for colours in man pages to work
+      GROFF_NO_SGR = "1";
+    };
 
-      alias ps "echo \"don't you mean procs(1)?\""
+    configFile = {
+      text = ''
+        $env.config = {
+          show_banner: false
+        }
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/nix/nix-completions.nu *
+      '';
+    };
 
-      # man colours
-      export LESS_TERMCAP_mb="$(tput bold; tput setaf 1)"
-      export LESS_TERMCAP_md="$(tput bold; tput setaf 1)"
-      export LESS_TERMCAP_me="$(tput sgr0)"
-      export LESS_TERMCAP_so="$(tput bold; tput setaf 12)"
-      export LESS_TERMCAP_se="$(tput sgr0)"
-      export LESS_TERMCAP_us="$(tput bold; tput setaf 14)"
-      export LESS_TERMCAP_ue="$(tput sgr0)"
-      # required for man colours to work
-      export GROFF_NO_SGR=1;
-    '';
-
-    loginShellInit = ''
-      if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
-          exec sx
-      end
+    loginFile.text = ''
+      if ($env.DISPLAY? | is-empty) and (not ($env.XDG_VTNR | is-empty)) { exec sx }
     '';
   };
 
@@ -504,7 +482,7 @@ in
     VISUAL = "hx";
 
     PAGER = "cat"; # NOTE: setting no pager breaks bad software
-    MANPAGER = "less";
+    MANPAGER = "less --mouse --wheel-lines 3";
     MANWIDTH = 72;
 
     EMAIL = "cjbdev@icloud.com";
